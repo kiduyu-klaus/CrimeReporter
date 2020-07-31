364b706c15fcdc181e6792110cda4ba002eeea41
kiduyu-klaus <muigakiduyu1995@gmail.com>
2020-07-31 00:40:59 -0700

    added splash

---
+++ app/src/main/java/com/kiduyu/kingooproject/crimereporter/MissingPersonsActivity.java
@@ -0,0 +1,14 @@
+package com.kiduyu.kingooproject.crimereporter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MissingPersonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_missing_persons);
+    }
+}
